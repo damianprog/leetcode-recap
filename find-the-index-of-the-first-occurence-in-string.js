b4ea9e1da2347b789c4cc4b21024ba326e1c2673
damianprog <damianpukszta@gmail.com>
2026-06-30 10:42:12 +0200

    Find the index of the first occurence in string classic brute force

--- find-the-index-of-the-first-occurence-in-string.js
+++ find-the-index-of-the-first-occurence-in-string.js
@@ -3,14 +3,28 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function (haystack, needle) {
-  for (let i = 0; i <= haystack.length - needle.length; i++) {
-    let j = i;
-    while (haystack[j] === needle[j - i]) {
-      if (j - i === needle.length - 1) return i;
-      j++;
-    }
-  }
+const strStr = function (haystack, needle) {
+  //   for (let i = 0; i <= haystack.length - needle.length; i++) {
+  //     let j = i;
+  //     while (haystack[j] === needle[j - i]) {
+  //       if (j - i === needle.length - 1) return i;
+  //       j++;
+  //     }
+  //   }
 
+  //   return -1;
+
+  // ==================================================================================
+  // Wersja trochę bardziej czytelna ale nadal O(n*m)
+
+  const n = haystack.length,
+    m = needle.length;
+  for (let i = 0; i <= n - m; i++) {
+    let k = 0;
+    while (k < m && haystack[i + k] === needle[k]) k++;
+    if (k === m) return i;
+  }
   return -1;
+
+  // ==================================================================================
 };
